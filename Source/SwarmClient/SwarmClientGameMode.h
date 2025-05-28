6791e3e467b768243479fdf0ef9286ce6bbd94c2
jeondoh <jundo1302@gmail.com>
2025-05-28 17:22:33 +0900

    feat: third Person 패키지 추가

---
+++ Source/SwarmClient/SwarmClientGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SwarmClientGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASwarmClientGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASwarmClientGameMode();
+};
+
+
+
