a867f8caf2adcbdfb1d36f7da9e0ce6115f6bffb
jeondoh <jundo1302@gmail.com>
2025-06-30 23:42:24 +0900

    feat: Player 게임 입장 및 스폰 로직 추가

--- Source/SwarmClient/Packet/PlayerPacketHandler.cpp
+++ Source/SwarmClient/Packet/PlayerPacketHandler.cpp
@@ -1,12 +1,33 @@
 #include "PlayerPacketHandler.h"
 
-// 캐릭터 입장 & 생성
-void FPlayerPacketHandler::Handle_SC_PLAYER_ENTER_GAME(FSessionRef Session, Protocol::SC_PLAYER_ENTER_GAME& Packet)
+#include "Zone/ZoneGameState.h"
+
+// 캐릭터 입장
+void FPlayerPacketHandler::Handle_SC_PLAYER_ENTER_GAME(FSessionRef Session, const Protocol::SC_PLAYER_ENTER_GAME& Packet)
+{
+	// GameState 가져오기
+	if (AZoneGameState* ZoneGameState = Cast<AZoneGameState>(World->GetGameState()))
+	{
+		// 플레이어 입장
+		ZoneGameState->EnterPlayer(Packet);
+	}
+}
+
+// 타 플레이어 스폰
+void FPlayerPacketHandler::Handle_SC_PLAYER_SPAWN(FSessionRef Session, const Protocol::SC_PLAYER_SPAWN& Packet)
+{
+	// GameState 가져오기
+	if (AZoneGameState* ZoneGameState = Cast<AZoneGameState>(World->GetGameState()))
+	{
+		// 플레이어 스폰
+		ZoneGameState->SpawnPlayer(Packet);
+	}
+}
+
+void FPlayerPacketHandler::Handle_SC_PLAYER_DESPAWN(FSessionRef Session, const Protocol::SC_PLAYER_DESPAWN& Packet)
 {
 }
 
-// 캐릭터 이동
-void FPlayerPacketHandler::Handle_SC_PLAYER_MOVE(FSessionRef Session, Protocol::SC_PLAYER_MOVE& Packet)
+void FPlayerPacketHandler::Handle_SC_PLAYER_MOVE(FSessionRef Session, const Protocol::SC_PLAYER_MOVE& Packet)
 {
-	
 }
