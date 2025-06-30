8b839154d6196d01b7241db1c7f4870a91fe9a80
jeondoh <jundo1302@gmail.com>
2025-06-30 23:40:00 +0900

    feat: 공통 오브젝트 클래스

---
+++ Source/SwarmClient/Character/GameObject.cpp
@@ -0,0 +1,73 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameObject.h"
+
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Zone/ZoneGameState.h"
+
+AGameObject::AGameObject()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	// Set size for collision capsule
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
+
+	// Don't rotate when the controller rotates. Let that just affect the camera.
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+	
+	// Configure character movement
+	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f); // ...at this rotation rate
+
+	// Note: For faster iteration times these variables, and many more, can be tweaked in the Character Blueprint
+	// instead of recompiling to adjust them
+	GetCharacterMovement()->JumpZVelocity = 420.f;
+	GetCharacterMovement()->AirControl = 0.35f;
+	GetCharacterMovement()->MaxWalkSpeed = 500.f;
+	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
+	GetCharacterMovement()->BrakingDecelerationFalling = 1500.0f;
+}
+
+// Object 정보 업데이트
+void AGameObject::UpdateObjectInfo(const Protocol::ObjectInfo& InObjInfo)
+{
+	Type = InObjInfo.type();
+	StatInfo.CopyFrom(InObjInfo.statinfo());
+	State = InObjInfo.state();
+	Name = InObjInfo.name();
+	UpdatePosition(InObjInfo.posinfo());
+}
+
+void AGameObject::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AGameObject::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// 위치 업데이트
+void AGameObject::UpdatePosition(const Protocol::PosInfo& InPosInfo)
+{
+	PosInfo.CopyFrom(InPosInfo);
+	World3D.UpdatePosition(InPosInfo);
+}
+
+// Player인지 확인
+bool AGameObject::IsPlayer() const
+{
+	return Type == Protocol::ObjectType::OBJECT_TYPE_PLAYER;
+}
+
+// Monster인지 확인
+bool AGameObject::IsMonster() const
+{
+	return Type == Protocol::ObjectType::OBJECT_TYPE_MONSTER;
+}
---
+++ Source/SwarmClient/Character/GameObject.h
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Struct.pb.h"
+#include "GameFramework/Character.h"
+#include "Zone/World3D.h"
+#include "GameObject.generated.h"
+
+class AZoneGameState;
+/*-------------------------------------------------------
+					AGameObject
+
+- 모든 오브젝트 공통 항목
+- AGameObject 상속받아 플레이어, 몬스터 등 구현
+--------------------------------------------------------*/
+UCLASS()
+class SWARMCLIENT_API AGameObject : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AGameObject();
+
+	// Object 정보 업데이트
+	void UpdateObjectInfo(const Protocol::ObjectInfo& InObjInfo);
+	// 위치 업데이트
+	void UpdatePosition(const Protocol::PosInfo& InPosInfo);
+	// Player인지 확인
+	bool IsPlayer() const;
+	// Monster인지 확인
+	bool IsMonster() const;
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// 고유 ID
+	uint64 ObjectId;
+	// 캐릭터 타입
+	Protocol::ObjectType Type;
+	// 위치 및 방향 정보
+	Protocol::PosInfo PosInfo;
+	FWorld3D World3D;
+	// 캐릭터 상태
+	Protocol::ObjectState State;
+	// 스탯 정보
+	Protocol::StatInfo StatInfo;
+	// 이름
+	std::string Name;
+};
