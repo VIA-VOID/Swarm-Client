5d4528875917bbdf9c5ae6c0f5729a6ba3d7113d
jeondoh <jundo1302@gmail.com>
2025-07-04 01:20:27 +0900

    feat: 디스폰 로직 추가

--- Source/SwarmClient/Packet/PlayerPacketHandler.cpp
+++ Source/SwarmClient/Packet/PlayerPacketHandler.cpp
@@ -24,8 +24,15 @@ void FPlayerPacketHandler::Handle_SC_PLAYER_SPAWN(FSessionRef Session, const Pro
 	}
 }
 
+// 플레이어 디스폰
 void FPlayerPacketHandler::Handle_SC_PLAYER_DESPAWN(FSessionRef Session, const Protocol::SC_PLAYER_DESPAWN& Packet)
 {
+	// GameState 가져오기
+	if (AZoneGameState* ZoneGameState = Cast<AZoneGameState>(World->GetGameState()))
+	{
+		// 플레이어 디스폰
+		ZoneGameState->DespawnPlayer(Packet.objectid());
+	}
 }
 
 void FPlayerPacketHandler::Handle_SC_PLAYER_MOVE(FSessionRef Session, const Protocol::SC_PLAYER_MOVE& Packet)
