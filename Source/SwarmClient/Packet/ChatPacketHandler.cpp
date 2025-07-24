c181b1be5afc093f294fed1dd825f8b6073a429e
jeondoh <jundo1302@gmail.com>
2025-07-24 23:55:59 +0900

    feat: 채팅 패킷 처리

--- Source/SwarmClient/Packet/ChatPacketHandler.cpp
+++ Source/SwarmClient/Packet/ChatPacketHandler.cpp
@@ -1,5 +1,19 @@
 #include "ChatPacketHandler.h"
 
+#include "ZoneGameState.h"
+#include "Player/SwarmMyPlayer.h"
+
+// 채팅
 void FChatPacketHandler::Handle_SC_CHAT_MSG(FSessionRef Session, const Protocol::SC_CHAT_MSG& Packet)
 {
+	// GameState 가져오기
+	if (AZoneGameState* ZoneGameState = Cast<AZoneGameState>(World->GetGameState()))
+	{
+		ASwarmMyPlayer* SwarmMyPlayer = Cast<ASwarmMyPlayer>(ZoneGameState->GetMyPlayer());
+		if (SwarmMyPlayer == nullptr)
+		{
+			return;
+		}
+		SwarmMyPlayer->RecvChatMessage(Packet);
+	}
 }
