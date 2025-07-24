e7b4e267ba6ae5cbb1b6b79382c9074066352e7c
jeondoh <jundo1302@gmail.com>
2025-07-24 23:55:34 +0900

    fix: RTT 절대값 적용

--- Source/SwarmClient/Packet/SystemPacketHandler.cpp
+++ Source/SwarmClient/Packet/SystemPacketHandler.cpp
@@ -1,18 +1,17 @@
 #include "SystemPacketHandler.h"
 #include "Network/Session.h"
-#include "SwarmGameInstance.h"
 #include "GameMacro.h"
 
 // 서버의 PING
 void FSystemPacketHandler::Handle_SC_SYSTEM_PING(FSessionRef Session, const Protocol::SC_SYSTEM_PING& Packet)
 {
-	const FDateTime Now = FDateTime::Now();
 	const int64 ServerTime = Packet.currenttime();
-	const int64 ClientTime = Now.GetTicks() / 10000;
+	const int64 ClientTime = std::chrono::duration_cast<std::chrono::milliseconds>(
+		std::chrono::system_clock::now().time_since_epoch()).count();
 	const int32 DiffTime = static_cast<int32>(ClientTime - ServerTime);
 
 	// RTT 업데이트
-	Session->UpdateRoundTripTime(DiffTime);
+	Session->UpdateRoundTripTime(FMath::Abs(DiffTime));
 
 	// PONG 패킷 전송
 	Protocol::CS_SYSTEM_PONG PongPkt;
